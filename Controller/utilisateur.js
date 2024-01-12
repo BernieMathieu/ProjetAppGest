d922a8216fa382a7babe1608427124b4c9025de1
BernieMathieu <ekomebernie@gmail.com>
2024-01-12 17:25:58 +0100

    deploiement2

--- Controller/utilisateur.js
+++ Controller/utilisateur.js
@@ -1,9 +1,9 @@
-const { User } = require("../Model/user")
+// const { User } = require("../Model/user")
 
-const ajouterUtilisateur = async (req, res)=>{
-    try {
-        let utilisateur = new User (req.body.id, req.body.noms, req.body.prenoms, req.body.sexe, req.body.téléphone, req.body.adressemail, req.body.datedenaissance, req.body.niveau, req.body.activiteextrascolaire );
-    } catch (error) {
+// const ajouterUtilisateur = async (req, res)=>{
+//     try {
+//         let utilisateur = new User (req.body.id, req.body.noms, req.body.prenoms, req.body.sexe, req.body.téléphone, req.body.adressemail, req.body.datedenaissance, req.body.niveau, req.body.activiteextrascolaire );
+//     } catch (error) {
         
-    }
-}
+//     }
+// }
